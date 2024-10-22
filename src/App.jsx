7ed41b7864e24fab78d1e988080cc58ba7732f86
Hera <mostafizhera@gmail.com>
2024-10-23 04:54:43 +0600

    Added the all dynamic data and showing successfull

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { useEffect } from "react"
+import Blog from "./Components/Blog/Blog"
 export default function App() {
 
   const [myData, setMyData] = useState([])
@@ -7,12 +8,30 @@ export default function App() {
   useEffect(()=>{
     fetch("blogs.json")
      .then(res=>res.json())
-     .then(data=>console.log(data))
+     .then(data=>{
+      setMyData(data)
+      console.log(data);
+      
+     })
+     
+     
     
   }, [])
   return (
     <>
-    <h2>This is App page here now.</h2>
+       <div className="container mx-auto">
+       <div className=" flex justify-between items-center py-5 ">
+        <div className=" text-2xl font-bold">Knowledge Cafe</div>
+        <div className=" w-[50px] h-[50px] bg-gradient-to-r from-blue-500 to-pink-500 p-[3px] rounded-full"><img className=" w-full h-full rounded-full" src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Profile image" /></div>
+       </div>
+       <hr />
+       </div>
+       <div className=" container mx-auto border-2 border-red-500 grid grid-cols-3 gap-5">
+          <div className=" col-span-2">{myData.map(item=> <Blog key={item.id} item={item}></Blog> )}</div>
+          <div>
+            <div className=" border-2 border-blue-700 rounded-xl p-6 bg-blue-50 text-center"><h2 className=" font-bold text-xl text-blue-700">Spent time on read : 177 min</h2></div>
+          </div>
+       </div>
     </>
   )
 }
--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
-
-export default function Blog() {
+import { FaRegBookmark } from "react-icons/fa";
+export default function Blog({item}) {
   return (
-    <div>Blog</div>
+    <>
+      <div className=' container max-auto border mb-10 bg-red-50 p-10 rounded-lg'>
+        <div>
+          <div><img className=' w-full object-cover rounded-lg' src={item.cover} alt="" /></div>
+          <div className=' flex justify-between py-5 items-center'>
+              <div className=' flex gap-3'>
+                  <div className=' w-[50px] h-[50px] rounded-full bg-gradient-to-r from-purple-500 to-pink-500 p-[3px]'><img className=' rounded-full w-full h-full object-cover' src={item.author_img} alt="author image" /></div>
+                  <div>
+                    <h2 className=' font-bold'>{item.author}</h2>
+                    <p>{item.posted_date}</p>
+                  </div>
+              </div>
+              {/* 1st div Close */}
+              <div>
+                  <div>
+                    <p className=' font-bold flex justify-center items-center gap-3'> <span>{item.reading_time}</span> <span className=' text-green-400 cursor-pointer hover:text-green-700'><FaRegBookmark /></span></p>
+                  </div>
+              </div>
+          </div>
+          {/* author info div Close */}
+          <h2 className=' text-3xl font-bold'>{item.title}</h2>
+          <p className=' py-3'>{item.hashtags}</p>
+          <button className=' text-[#6047EC] hover:text-[#7a66f0] font-semibold underline'>Mark as read</button>
+          <div className=' mt-3'><hr /></div>
+        </div>
+      </div>
+    </>
   )
 }
